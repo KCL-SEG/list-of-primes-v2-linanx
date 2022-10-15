7bb9bd6bb2b1ae583fffade67cf14e82d2d706bb
Lina Namoun <76211897+linanx@users.noreply.github.com>
2022-10-15 18:11:34 +0100

    Complete task

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError(f"Number of primes {number_of_primes}: should have been greater than 0.")
+
     list = []
+    index = 2
+    while len(list) < number_of_primes:
+        if len(list) == 0:
+            list.append(index)
+        else:
+            for p in list:
+                if index % p == 0:
+                    break
+            else:
+                list.append(index)
+        index += 1
     return list
